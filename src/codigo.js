32a8357dea0f8893d1795163cdebe8d44211b6f2
Armuto-Live <luisarmuto3@gmail.com>
2024-03-14 14:27:40 -0500

    completado

--- src/codigo.js
+++ src/codigo.js
@@ -5,41 +5,87 @@ const $resultadoTitulo = d.querySelector(".mensaje-titulo");
 const $mensajeDescripcion = d.querySelector(".mensaje-descripcion");
 const $botonCopiar = d.querySelector(".boton-copiar");
 
+let textoMinuscula = false;
+
+const cifrarPalabra = (palabraACifrar) => {
+  const cifradoMap = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+  };
+  const palabraCrifrada = palabraACifrar
+    .split("")
+    .map((letra) => cifradoMap[letra] || letra)
+    .join("");
+
+  return palabraCrifrada;
+};
+
+const descifrarPalabra = (palabraCifrada) => {
+  for (const palabra in palabraCifrada.split(" ")) {
+    palabraCifrada = palabraCifrada.replace(/ai/, "a");
+    palabraCifrada = palabraCifrada.replace(/enter/, "e");
+    palabraCifrada = palabraCifrada.replace(/imes/, "i");
+    palabraCifrada = palabraCifrada.replace(/ober/, "o");
+    palabraCifrada = palabraCifrada.replace(/ufat/, "u");
+  }
+
+  return palabraCifrada;
+};
+
+const inputVacio = () => {
+  $resultadoTitulo.textContent = "Ningún mensaje fue encontrado";
+  $mensajeDescripcion.textContent =
+    "Ingresa el texto que desees encriptar o desencriptar.";
+};
+
+const validarTexto = (texto) => {
+  const formato = /^[a-z]+$/;
+  if (formato.test(texto)) {
+    textoMinuscula = true;
+  } else {
+    alert("Solo debes ingresar texto en minuscula");
+    textoMinuscula = false;
+  }
+  console.log(textoMinuscula);
+  return textoMinuscula;
+};
 d.addEventListener("click", (e) => {
   if (e.target.matches(".boton-encriptar")) {
     if ($textTarea.value.length === 0) {
-      alert("Debes ingresar un texto");
+      inputVacio();
+      $botonCopiar.setAttribute("type", "hidden");
+    } else if (!validarTexto($textTarea.value)) {
       $resultadoTitulo.textContent = "Ningún mensaje fue encontrado";
       $mensajeDescripcion.textContent =
         "Ingresa el texto que desees encriptar o desencriptar.";
-      $botonCopiar.setAttribute("type", "hidden");
     } else {
       $resultadoTitulo.textContent = "";
-      const palabraACifrar = $textTarea.value.split("");
-      
-      for (let letra in palabraACifrar) {
-        if (palabraACifrar[letra] === "a") {
-            palabraACifrar[letra] = "ai";
-        }
-        if (palabraACifrar[letra] === "e") {
-            palabraACifrar[letra] = "enter";
-        }
-        if (palabraACifrar[letra] === "i") {
-            palabraACifrar[letra] = "imes";
-        }
-        if (palabraACifrar[letra] === "o") {
-            palabraACifrar[letra] = "ober";
-        }
-        if (palabraACifrar[letra] === "u") {
-            palabraACifrar[letra] = "ufat";
-        }
-      }
-      
+
       $botonCopiar.setAttribute("type", "button");
-
-      $mensajeDescripcion.textContent = palabraACifrar.join('');
-
+      $mensajeDescripcion.textContent = cifrarPalabra($textTarea.value);
     }
   }
 
+  if (e.target.matches(".boton-desencriptar")) {
+    if ($textTarea.value.length === 0) {
+      inputVacio();
+      $botonCopiar.setAttribute("type", "hidden");
+    } else if (!validarTexto($textTarea.value)) {
+      $resultadoTitulo.textContent = "Ningún mensaje fue encontrado";
+      $mensajeDescripcion.textContent =
+        "Ingresa el texto que desees encriptar o desencriptar.";
+    } else {
+      $resultadoTitulo.textContent = "";
+
+      $botonCopiar.setAttribute("type", "button");
+      $mensajeDescripcion.textContent = descifrarPalabra($textTarea.value);
+    }
+  }
+
+  if (e.target.matches(".boton-copiar")) {
+    navigator.clipboard.writeText($mensajeDescripcion.textContent);
+  }
 });
